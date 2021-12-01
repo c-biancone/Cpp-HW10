dfee17f6ef6129fe18b83ee32eb4a69e79d4b42a
c-biancone <chris.biancone@gmail.com>
2021-12-01 16:12:39 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Homework_10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_10.dir/main.cpp.o"
+  "Homework_10.exe"
+  "Homework_10.pdb"
+  "libHomework_10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "ChatSession.h"
+
+using namespace std;
+const bool ECHO_INPUT = true;
+
+int main()
+{
+
+    /*
+     * Input to setup Chat Session
+     * USERNAME
+     * BOTNAME_1
+     * BOTNAME_2
+     * ....
+     * START
+     */
+
+    /*
+     * Example:
+     * Neo
+     * T800
+     * START
+     */
+
+    string userName;
+    string botName;
+
+    // Read in username and create the chat sesssion
+
+    // Register the chat bots
+    while(botName != "START")
+    {
+    }
+
+    // Run the chat session
+
+    return 0;
+}
